f8ba4e1d025b372ed110ee7b1bbab2d8491cf233
MinSung_Jo <152842047+MinSung0732@users.noreply.github.com>
2026-09-23 01:33:31 +0900

    Support gunner cylinder slow

--- src/monsters/bomb_rat.gd
+++ src/monsters/bomb_rat.gd
@@ -76,7 +76,10 @@ func _physics_process(delta: float) -> void:
 
 	var distance := global_position.distance_to(hero.global_position)
 	if distance > self_destruct_range:
-		velocity = direction_to_hero * move_speed
+		var external_slow := 1.0
+		if int(get_meta("gunner_slow_until", 0)) > Time.get_ticks_msec():
+			external_slow = clampf(float(get_meta("gunner_slow_multiplier", 1.0)), 0.1, 1.0)
+		velocity = direction_to_hero * move_speed * external_slow
 		_play_locomotion(true)
 		move_and_slide()
 	else:
