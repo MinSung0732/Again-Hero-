6dee156ee6b431fe353c9c4c916ad2caa0243fd0
MinSung_Jo <152842047+MinSung0732@users.noreply.github.com>
2026-09-23 01:33:26 +0900

    Support gunner cylinder slow

--- src/monsters/orc.gd
+++ src/monsters/orc.gd
@@ -41,9 +41,12 @@ func _physics_process(delta: float) -> void:
 	last_charge_timer = maxf(last_charge_timer - delta, 0.0)
 
 	var combat_bonuses := _get_combat_bonuses()
+	var external_slow := 1.0
+	if int(get_meta("gunner_slow_until", 0)) > Time.get_ticks_msec():
+		external_slow = clampf(float(get_meta("gunner_slow_multiplier", 1.0)), 0.1, 1.0)
 	var effective_move_speed := move_speed * float(
 		combat_bonuses.get("move_speed_multiplier", 1.0)
-	)
+	) * external_slow
 	var effective_attack_cooldown := attack_cooldown / maxf(
 		float(combat_bonuses.get("attack_speed_multiplier", 1.0)),
 		0.01
